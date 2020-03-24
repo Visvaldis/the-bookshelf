bd7d180e08c16975a4fc824753ce272e2c6231dd
Vsevolod Slavinskiy <sevoslav@gmail.com>
2020-03-24 15:26:23 +0200

    Started work with repositories

---
+++ TheBookshelf.DAL/Repositories/BookRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TheBookshelf.DAL.Context;
+using TheBookshelf.DAL.Entities;
+using TheBookshelf.DAL.Interfaces;
+
+namespace TheBookshelf.DAL.Repositories
+{
+	class BookRepository : IRepository<Book>
+	{
+		private readonly BookshelfContext db;
+		public BookRepository(BookshelfContext context)
+		{
+			this.db = context;
+		}
+		public void Create(Book item)
+		{
+			db.Books.Add(item);
+		}
+
+		public void Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IQueryable<Book> Find(Expression<Func<Book, bool>> predicate)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Book Get(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Book> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Book item)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
