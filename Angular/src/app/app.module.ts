a375c737693015dfdd7b576c216c3fc185d1a5ee
Vsevolod <sevoslav@gmail.com>
2020-06-30 18:42:36 +0300

    Created footer component. Started wor with header and tags components. Added simple navigation.

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import {Routes, RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { TagsComponent } from './tags/tags.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {ApiConfig} from '../apiConfig';
+
+// определение маршрутов
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'tags', component: TagsComponent},
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiConfig],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
