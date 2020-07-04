a850e679e59e7fb17572b7d68207018c7829aa0e
Vsevolod <sevoslav@gmail.com>
2020-07-05 00:35:12 +0300

    Modified footer

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {TagsComponent} from './components/tags/tags.component';
+import {LoginComponent} from './auth/components/login/login.component';
+import {RegisterComponent} from './auth/components/register/register.component';
 
-
-const routes: Routes = [];
+// определение маршрутов
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'tags', component: TagsComponent},
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forChild(routes),
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
